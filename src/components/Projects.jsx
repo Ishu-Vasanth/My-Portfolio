810407d9161b2b036e972389326b1437feb29b4e
Your Name <paarkavy.bv@gmail.com>
2024-07-17 11:50:18 +0530

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>Projects</h2>
+    <ul>
+      <li>I did a project on "FOOD FORECAST: An Innovative Food Prediction System" which is used for optimizing inventory levels, and predicting potential food wastage, it substantially curtails food wastage and reduces inventory costs.</li>
+      <li>Mini project on "Animal Disease Classification" using RESNET and Machine learning.This project helps the terrace farmers to identify the specific disease that has affected their crop.</li>
+    </ul>
+  </section>
+);
+export default Projects;
